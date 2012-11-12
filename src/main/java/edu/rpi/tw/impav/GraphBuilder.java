93540109e3bc4c3f1783ebc7b366f295c747cf06
Jim McCusker <mccusker@gmail.com>
2012-11-11 19:05:08 -0500

    added correct location.

--- src/main/java/edu/rpi/tw/impav/GraphBuilder.java
+++ src/main/java/edu/rpi/tw/impav/GraphBuilder.java
@@ -97,7 +97,7 @@ public class GraphBuilder implements Runnable {
                     if(t.location.compareTo("null") < 0){
                         query = query + "  <http://purl.org/twc/skitter/"+ t.creator.getScreenName() + "/status/" + t.id +"/location> a geo:Point; \n"+
                             "     " + t.location + "\n"+
-                            "  <http://purl.org/twc/skitter/"+ t.creator.getScreenName() + "/status/" + t.id +"> prov:location <http://purl.org/twc/skitter/"+ t.creator.getScreenName() + "/status/" + t.id +">.\n";
+                            "  <http://purl.org/twc/skitter/"+ t.creator.getScreenName() + "/status/" + t.id +"> prov:atLocation <http://purl.org/twc/skitter/"+ t.creator.getScreenName() + "/status/" + t.id +">.\n";
                         //request.add("prefix dc: <http://purl.org/dc/terms/> prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> prefix prov: <http://www.w3.org/ns/prov#> prefix ogc: <http://www.opengis.net/rdf#> prefix geo: <http://www.w3.org/2003/01/geo/wgs84_pos#> prefix foaf: <http://xmlns.com/foaf/0.1/> prefix owl: <http://www.w3.org/2002/07/owl#> prefix xsd: <http://www.w3.org/2001/XMLSchema#> prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> INSERT DATA{ <http://purl.org/twc/skitter/tweet/"+ count +"> dc:subject <http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#" + label + ">; prov:value \"" + t.text + "\"; rdfs:seeAlso <http://twitter.com/" + t.creator + "/status/" + t.id + ">; dc:date \"" + t.created + "\"^^xsd:dateTime . <http://purl.org/twc/skitter/tweet/" + count + "/location> a geo:Point;"+ t.location + "<http://purl.org/twc/skitter/tweet/" + count + "> prov:location <http://purl.org/twc/skitter/tweet/" + count + "/location>.}" );
 
                     }else{
